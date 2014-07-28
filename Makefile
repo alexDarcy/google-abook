72ff4714d580023c240579ae65886ba49360bb16
Alexis Praga <alexis.praga@cerfacs.fr>
2014-07-28 14:20:45 +0200

    Reading line into buffer and store the parsing result directly into a struct. File format at the moment : name=XXXX email=XXX

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ CXX=g++
 
 consume-abook: consume-abook.o
 	${CXX} -o $@ $<
+
+clean:
+	rm -f *.o consume-abook
--- consume-abook.cpp
+++ consume-abook.cpp
@@ -1,18 +1,37 @@
+//#define BOOST_SPIRIT_DEBUG
 #include <boost/config/warning_disable.hpp>
 #include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
-//#include <boost/lambda/lambda.hpp>
-//#include <boost/bind.hpp>
+#include <boost/spirit/include/phoenix_object.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/io.hpp>
 
 #include <iostream>
 #include <fstream>
 
-namespace client
+/* Structure to store data */
+namespace entity 
+{
+  struct person
+  {
+    std::string name;
+    std::string email;
+  };
+}
+
+BOOST_FUSION_ADAPT_STRUCT(
+    entity::person,
+    (std::string, name)
+    (std::string, email)
+)
+
+namespace entity
 {
   namespace qi = boost::spirit::qi;
   namespace ascii = boost::spirit::ascii;
   template <typename Iterator>
-    struct entry_parser : qi::grammar<Iterator>
+    struct entry_parser : qi::grammar<Iterator, person()>
   {
     entry_parser() : entry_parser::base_type(start)
     {
@@ -20,40 +39,50 @@ namespace client
       using qi::lit;
       using qi::_val;
       using qi::_1;
+      using qi::eol;
       using ascii::char_;
 
-      start = + (char_ - "=") >> "=" >> +(char_);
+      start %= 
+        "name=" >> + (char_ - eol) >> eol >> "email=" >> + (char_);
     }
 
-    qi::rule<Iterator> start;
+    qi::rule<Iterator, person()> start;
   };
 
 }
 
 int main() {
   typedef std::string::const_iterator iterator_type;
-  typedef client::entry_parser<iterator_type> entry_parser;
+  typedef entity::entry_parser<iterator_type> entry_parser;
   entry_parser g; // Our grammar
-  std::string str;
-  unsigned result;
-  std::ifstream file("test.dat");
+  std::string str1, str2;
+  entity::person pers; // Struct to save data
+  std::ifstream file("test.dat", std::ios_base::in);
 
-  if (file.is_open()){
-    while (getline(file, str)){
-      std::cout << "Reading " << str << "...";
-      std::string::const_iterator iter = str.begin();
-      std::string::const_iterator end = str.end();
-
-      bool r = parse(iter, end, g, result);
-      if (r && iter == end) 
-        std::cout << "ok" <<  std::endl;
-      else
-        std::cout << "failed" << std::endl;
-      }
-      file.close();
-    }
-    else
-      std::cout << "Failed to open file" << std::endl;
-
-    return 0;
+  if (!file) {
+    std::cout << "Failed to open file" << std::endl;
+    return 1;
   }
+
+
+  // Read file into buffer
+  std::string buffer; 
+  file.unsetf(std::ios::skipws); // No white space skipping!
+  std::copy(
+      std::istream_iterator<char>(file),
+      std::istream_iterator<char>(),
+      std::back_inserter(buffer));
+
+  std::string::const_iterator iter = buffer.begin();
+  std::string::const_iterator end = buffer.end();
+
+  bool r = parse(iter, end, g, pers);
+  if (r && iter == end) {
+    std::cout << "ok" <<  std::endl;
+    std::cout << "got: " << boost::fusion::as_vector(pers) << std::endl;
+  }
+  else
+    std::cout << "failed" << std::endl;
+
+  return 0;
+}
